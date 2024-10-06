04da917a733c4c4a6190b6af6f1771778d40498c
Mark Condello <mark.condello@brightlabs.com.au>
2024-10-06 18:56:58 +1100

    included sqlite3 to save visitors to memory

--- index-ws.js
+++ index-ws.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { get } = require('http')
 const server = require('http').createServer()
 const app = express()
 
@@ -9,6 +10,17 @@ app.get('/', function(req, res) {
 server.on('request', app)
 server.listen(3000, function() {
     console.log('Express server listening on port 3000')
+})
+
+process.on('SIGINT', function() {
+    wss.clients.forEach(function each(client) { // close all websocket connections
+        client.send('Server is shutting down.')
+        client.close()
+        
+    })
+   server.close(function(){
+    shutdownDB() // close the database
+   }) 
 })
 
 // Begin Websocker server
@@ -24,6 +36,10 @@ wss.on('connection', function connection(ws) {
     if (ws.readyState === ws.OPEN) {
         ws.send('Welcome to the websocker server!')
     }
+// store visitors count in database
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))`)
+
     ws.on('close', function close() {
         wss.broadcast(message) // Broadcast to all clients in wss
         console.log('A client disconnected')
@@ -34,4 +50,27 @@ wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
         client.send(data)
     })
+}
+// End Websocker server
+// Begin Database server
+const sqlite = require('sqlite3')
+const db = new sqlite.Database(':memory:') // we can save to a file instead of memory
+
+db.serialize(function() { // check db exists before writting to it
+    db.run(`CREATE TABLE visitors (
+        count INTEGER,
+        time TEXT
+    )`)
+})
+
+function getCounts(){
+    db.each(`SELECT * FROM visitors`, function(err, row) {
+        console.log(row)
+    })
+}
+
+function shutdownDB(){
+    getCounts()
+    console.log('Closing database')
+    db.close()
 }
