120981218cc92392cc8d58784b5516560ea2342f
Mark Condello <mark.condello@brightlabs.com.au>
2024-10-06 15:24:48 +1100

    Included websocket server

---
+++ index-ws.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const server = require('http').createServer()
+const app = express()
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: __dirname })
+})
+
+server.on('request', app)
+server.listen(3000, function() {
+    console.log('Express server listening on port 3000')
+})
+
+// Begin Websocker server
+const WebSocketServer = require('ws').Server
+const wss = new WebSocketServer({ server: server })
+
+// Handle incoming WebSocket connections event
+wss.on('connection', function connection(ws) {
+    const numClients = wss.clients.size
+    const message = `Current visitors: ${numClients}`
+    console.log({message})
+    wss.broadcast(message) // Broadcast to all clients in wss
+    if (ws.readyState === wss.OPEN) {
+        ws.send('Welcome to the websocker server!')
+    }
+    ws.on('close', function close() {
+        wss.broadcast(message) // Broadcast to all clients in wss
+        console.log('A client disconnected')
+    })
+})
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data)
+    })
+}
